868249c1833336dce5d9ad4e883bfb7c67a019f3
Mai-Lapyst <info@lapyst.by>
2023-11-21 11:13:05 +0100

    Parameterize jei & botania versions; update JEI compat

--- forge/src/main/java/owmii/losttrinkets/client/compat/jei/JEI.java
+++ forge/src/main/java/owmii/losttrinkets/client/compat/jei/JEI.java
@@ -5,6 +5,7 @@ import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
@@ -15,7 +16,15 @@ import owmii.losttrinkets.handler.UnlockManager;
 public class JEI implements IModPlugin {
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        UnlockManager.getTrinkets().stream().map(ItemStack::new).forEach(stack -> registration.addIngredientInfo(stack, VanillaTypes.ITEM, Util.createTranslationKey("info", Registry.ITEM.getId(stack.getItem()))));
+        UnlockManager.getTrinkets().stream()
+            .map(ItemStack::new)
+            .forEach(stack -> {
+                Identifier key = Registry.ITEM.getId(stack.getItem());
+                registration.addIngredientInfo(
+                    stack, VanillaTypes.ITEM,
+                    new TranslatableText(Util.createTranslationKey("info", key))
+                );
+            });
     }
 
     @Override
